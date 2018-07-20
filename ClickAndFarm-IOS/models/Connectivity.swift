98e03c71bd5593f15ff605c429d1ed358a8e7db7
Jonathan De Haay <jdehaay@ekylibre.com>
2018-07-20 10:09:54 +0200

    Adding login scene to complete regrouping
    Adding login screen controller, use a simple database to check authentification. Change scene if user's account is autorized.

---
+++ ClickAndFarm-IOS/models/Connectivity.swift
@@ -0,0 +1,32 @@
+//
+//  Connectivity.swift
+//  ClickAndFarm-IOS
+//
+//  Created by Jonathan DE HAAY on 17/07/2018.
+//  Copyright © 2018 Ekylibre. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+class Connectivity {
+    class func isConnectedToInternet() -> Bool {
+        var zeroAddress = sockaddr_in()
+
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        var flags = SCNetworkReachabilityFlags()
+        
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
+}
---
+++ ClickAndFarm-IOS/views/RoundedButton.swift
@@ -0,0 +1,23 @@
+//
+//  RoundedButton.swift
+//  ClickAndFarm-IOS
+//
+//  Created by Jonathan DE HAAY on 16/07/2018.
+//  Copyright © 2018 Ekylibre. All rights reserved.
+//
+
+import UIKit
+
+class roundedButton: UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5
+        layer.backgroundColor = UIColor(red: 214/255.0, green: 215/255.0, blue: 215/255.0, alpha: 1.0).cgColor
+        layer.masksToBounds = false
+        layer.shadowRadius = 1
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.shadowOpacity = 0.6
+        contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
+    }
+}
