9678a0dd48f9e7d678bc2fa9c40b0a2f850aaacc
Guillaume Roux <groux@ekylibre.com>
2018-07-23 10:54:53 +0200

    Missing core data
    Recreating project on xcode, changing name and adding core data

---
+++ Clic&Farm-iOS/Clic&Farm-iOS/Crop.swift
@@ -0,0 +1,15 @@
+//
+//  Crop.swift
+//  ClickAndFarm-IOS
+//
+//  Created by Guillaume Roux on 20/07/2018.
+//  Copyright © 2018 Ekylibre. All rights reserved.
+//
+
+import Foundation
+
+class Crop {
+    
+    //MARK: Properties
+    
+}
