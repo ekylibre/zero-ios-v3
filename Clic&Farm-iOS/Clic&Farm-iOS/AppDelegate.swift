59cee75e60b684e6cdc9a2730d4a3e7313758f6d
Jonathan De Haay <jdehaay@ekylibre.com>
2018-07-24 09:51:24 +0200

    Create database using CoreData
    Create function to add new users to the table, a empty table function and a function to fetch the data. Added a simple user to the database

--- Clic&Farm-iOS/Clic&Farm-iOS/AppDelegate.swift
+++ Clic&Farm-iOS/Clic&Farm-iOS/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -72,7 +71,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
-
+    
     // MARK: - Core Data Saving support
 
     func saveContext () {
@@ -80,6 +79,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if context.hasChanges {
             do {
                 try context.save()
+                print("Saved")
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -88,6 +88,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
-
