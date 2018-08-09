5d314814fe72f25c6f6eb091086288e8947f4146
TontonBuck <tontonbuck@live.fr>
2018-08-09 13:53:01 +0200

    Removed constant.swift, added colorconstants, containing custom colors used in the app. Edit cells models

---
+++ Constants/ColorConstants.swift
@@ -0,0 +1,9 @@
+//
+//  ColorConstants.swift
+//  Clic&Farm-iOS
+//
+//  Created by Guillaume Roux on 09/08/2018.
+//  Copyright © 2018 Ekylibre. All rights reserved.
+//
+
+import Foundation
---
+++ Models/CustomCropCell.swift
@@ -0,0 +1,9 @@
+//
+//  CustomCropCell.swift
+//  Clic&Farm-iOS
+//
+//  Created by Guillaume Roux on 08/08/2018.
+//  Copyright © 2018 Ekylibre. All rights reserved.
+//
+
+import Foundation
