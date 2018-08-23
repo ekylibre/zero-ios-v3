8b63b053e153fc69170d8d0ebdbd02055ff78e9d
Jonathan De Haay <jdehaay@ekylibre.com>
2018-08-23 11:00:03 +0200

    Edit quantity measure selection, use dropDown to show list.

--- Models/SelectedInputsTableViewCell.swift
+++ Models/SelectedInputsTableViewCell.swift
@@ -7,21 +7,22 @@
 //
 
 import UIKit
+import DropDown
 
-class SelectedInputsTableViewCell: UITableViewCell, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+class SelectedInputsTableViewCell: UITableViewCell, UITextFieldDelegate {
   let inputImage = UIImageView()
   let inputName = UILabel()
   let inputType = UILabel()
   let inputQuantity = UITextField()
   let quantity = UILabel()
-  let quantityMeasure = UIView()
-  //let quantityMeasure = MeasureTableView(frame: CGRect(x: 0, y: 0, width: 60, height: 25))
+  let quantityMeasureButton = UIButton()
   let removeCell = UIButton()
   let surfaceQuantity = UILabel()
   let warningImage = UIImageView()
   let warningLabel = UILabel()
-  let measureType = [["g", "g/ha", "g/m2", "kg", "kg/ha", "kg/m3", "q", "q/ha", "q/m2", "t", "t/ha", "t/m2"], ["l", "l/ha", "l/m2", "hl", "hl/ha", "hl/m2", "m3", "m3/ha", "m3/m2"]]
-  var measureTableView: UITableView!
+  let quantityMeasureDropDown = DropDown()
+  let measureType = ["l", "l/ha", "l/m2", "hl", "hl/ha", "hl/m2", "m3", "m3/ha", "m3/m2"]
+  //var measureTableView: UITableView!
 
   override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -31,7 +32,7 @@ class SelectedInputsTableViewCell: UITableViewCell, UITextFieldDelegate, UITable
     inputType.translatesAutoresizingMaskIntoConstraints = false
     inputQuantity.translatesAutoresizingMaskIntoConstraints = false
     quantity.translatesAutoresizingMaskIntoConstraints = false
-    quantityMeasure.translatesAutoresizingMaskIntoConstraints = false
+    quantityMeasureButton.translatesAutoresizingMaskIntoConstraints = false
     removeCell.translatesAutoresizingMaskIntoConstraints = false
     surfaceQuantity.translatesAutoresizingMaskIntoConstraints = false
     warningImage.translatesAutoresizingMaskIntoConstraints = false
@@ -40,17 +41,19 @@ class SelectedInputsTableViewCell: UITableViewCell, UITextFieldDelegate, UITable
     inputName.font = UIFont.systemFont(ofSize: 14)
     inputType.font = UIFont.boldSystemFont(ofSize: 14)
     quantity.font = UIFont.systemFont(ofSize: 13)
+    quantityMeasureButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
     surfaceQuantity.font = UIFont.systemFont(ofSize: 13)
     warningLabel.font = UIFont.systemFont(ofSize: 13)
     quantity.textColor = AppColor.TextColors.DarkGray
+    quantityMeasureButton.setTitleColor(AppColor.TextColors.Black, for: .normal)
     inputQuantity.backgroundColor = AppColor.CellColors.white
     inputQuantity.layer.borderColor = UIColor.lightGray.cgColor
     inputQuantity.layer.borderWidth = 1
     inputQuantity.layer.cornerRadius = 5
-    quantityMeasure.backgroundColor = AppColor.CellColors.white
-    quantityMeasure.layer.borderColor = UIColor.lightGray.cgColor
-    quantityMeasure.layer.borderWidth = 1
-    quantityMeasure.layer.cornerRadius = 5
+    quantityMeasureButton.backgroundColor = AppColor.CellColors.white
+    quantityMeasureButton.layer.borderColor = UIColor.lightGray.cgColor
+    quantityMeasureButton.layer.borderWidth = 1
+    quantityMeasureButton.layer.cornerRadius = 5
     surfaceQuantity.textColor = AppColor.TextColors.DarkGray
     warningLabel.textColor = AppColor.TextColors.Red
     warningLabel.text = "dose invalide"
@@ -64,12 +67,16 @@ class SelectedInputsTableViewCell: UITableViewCell, UITextFieldDelegate, UITable
     inputQuantity.delegate = self
     inputQuantity.keyboardType = .numberPad
 
+    quantityMeasureButton.addTarget(self, action: #selector(self.showQuantityMeasure(sender:)), for: .touchUpInside)
+    customizeDropDown(sender: self)
+    setupQuantityMeasureDropDown()
+
     contentView.addSubview(inputImage)
     contentView.addSubview(inputName)
     contentView.addSubview(inputType)
     contentView.addSubview(inputQuantity)
     contentView.addSubview(quantity)
-    contentView.addSubview(quantityMeasure)
+    contentView.addSubview(quantityMeasureButton)
     contentView.addSubview(removeCell)
     contentView.addSubview(surfaceQuantity)
     contentView.addSubview(warningImage)
@@ -81,7 +88,7 @@ class SelectedInputsTableViewCell: UITableViewCell, UITextFieldDelegate, UITable
       "inputType": inputType,
       "inputQuantity": inputQuantity,
       "quantity": quantity,
-      "quantityMeasure": quantityMeasure,
+      "quantityMeasureButton": quantityMeasureButton,
       "removeCell": removeCell,
       "surfaceQuantity": surfaceQuantity,
       "warningImage": warningImage,
@@ -90,10 +97,10 @@ class SelectedInputsTableViewCell: UITableViewCell, UITextFieldDelegate, UITable
 
     contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[inputImage(==30)]-[inputName]-[inputType]", options: [], metrics: nil, views: viewsDict))
     contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[removeCell(==15)]-10-|", options: [], metrics: nil, views: viewsDict))
-    contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[quantity]-10-[inputQuantity(==100)][quantityMeasure(==60)]", options: [], metrics: nil, views: viewsDict))
+    contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[quantity]-10-[inputQuantity(==100)][quantityMeasureButton(==60)]", options: [], metrics: nil, views: viewsDict))
     contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-5-[inputImage(==30)]-10-[quantity]", options: [], metrics: nil, views: viewsDict))
-    contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[inputName]-15-[inputQuantity(==25)]", options: [], metrics: nil, views: viewsDict))
-    contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[inputType]-15-[quantityMeasure(==25)]", options: [], metrics: nil, views: viewsDict))
+    contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[inputName]-12.5-[inputQuantity(==30)]", options: [], metrics: nil, views: viewsDict))
+    contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[inputType]-12.5-[quantityMeasureButton(==30)]", options: [], metrics: nil, views: viewsDict))
     contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[removeCell(==15)]", options: [], metrics: nil, views: viewsDict))
     contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[warningImage(==10)]", options: [], metrics: nil, views: viewsDict))
     contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[warningImage(==10)]", options: [], metrics: nil, views: viewsDict))
@@ -107,24 +114,49 @@ class SelectedInputsTableViewCell: UITableViewCell, UITextFieldDelegate, UITable
       NSLayoutConstraint(item: warningLabel, attribute: .top, relatedBy: .equal, toItem: inputQuantity, attribute: .bottom, multiplier: 1, constant: 2).isActive = true
       NSLayoutConstraint(item: surfaceQuantity, attribute: .top, relatedBy: .equal, toItem: warningImage, attribute: .bottom, multiplier: 1, constant: 1).isActive = true
     }
-    let measureTypeSelection = MeasureTableView(frame: quantityMeasure.frame)
-
-    quantityMeasure.addSubview(measureTypeSelection)
-    measureTableView = measureTypeSelection.subviews.last as! UITableView
-    measureTableView.register(MeasureTableViewCell.self, forCellReuseIdentifier: "MeasureTableViewCell")
-    measureTableView.delegate = self
-    measureTableView.dataSource = self
   }
 
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return measureType[1].count
+  @objc func showQuantityMeasure(sender: UIButton) {
+    quantityMeasureDropDown.show()
   }
 
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = measureTableView.dequeueReusableCell(withIdentifier: "MeasureTableViewCell", for: indexPath) as! MeasureTableViewCell
+  func customizeDropDown(sender: AnyObject) {
+    let appearence = DropDown.appearance()
 
-    cell.measureType.text = measureType[indexPath.row][1]
-    return cell
+    appearence.cellHeight = 30
+    appearence.backgroundColor = AppColor.CellColors.white
+    appearence.selectionBackgroundColor = AppColor.CellColors.lightGray
+    appearence.cornerRadius = 2
+    appearence.shadowColor = UIColor(white: 0.6, alpha: 1)
+    appearence.shadowOpacity = 1
+    appearence.shadowRadius = 5
+    appearence.animationduration = 0.25
+    appearence.textColor = AppColor.TextColors.Black
+    appearence.textFont = UIFont.boldSystemFont(ofSize: 12)
+  }
+
+  func setupQuantityMeasureDropDown() {
+    quantityMeasureDropDown.anchorView = quantityMeasureButton
+    quantityMeasureDropDown.bottomOffset = CGPoint(x: 0, y: quantityMeasureButton.bounds.height)
+    quantityMeasureDropDown.direction = .bottom
+    quantityMeasureDropDown.dismissMode = .automatic
+    quantityMeasureDropDown.dataSource = [
+      "g",
+      "g/ha",
+      "g/m2",
+      "kg",
+      "kg/ha",
+      "kg/m3",
+      "q",
+      "q/ha",
+      "q/m2",
+      "t",
+      "t/ha",
+      "t/m2"
+    ]
+    quantityMeasureDropDown.selectionAction = { [weak self] (index, item) in
+      self?.quantityMeasureButton.setTitle(item, for: .normal)
+    }
   }
 
   func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
@@ -138,64 +170,6 @@ class SelectedInputsTableViewCell: UITableViewCell, UITextFieldDelegate, UITable
 
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-}
-
-class MeasureTableView: UIView {
-  var tableView: UITableView!
-
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-
-    tableView = UITableView(frame: CGRect.zero)
-    tableView.separatorInset = UIEdgeInsets.zero
-    let frame = CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 1 / UIScreen.main.scale)
-    let line = UIView(frame: frame)
-
-    line.backgroundColor = tableView.separatorColor
-    tableView.tableHeaderView = line
-    tableView.tableFooterView = UIView()
-    tableView.backgroundColor = AppColor.CellColors.white
-    tableView.layer.shadowColor = UIColor.black.cgColor
-    tableView.layer.shadowOffset = .zero
-    tableView.layer.shadowOpacity = 10
-    tableView.layer.shadowRadius = 5
-    tableView.layer.cornerRadius = 5
-    tableView.translatesAutoresizingMaskIntoConstraints = false
-
-    self.addSubview(tableView)
-    let viewDict = ["tableView": tableView] as [String: Any]
-
-    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[tableView]|", options: [], metrics: nil, views: viewDict))
-  }
-
-  @objc func reloadTable() {
-    tableView.reloadData()
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-}
-
-class MeasureTableViewCell: UITableViewCell {
-  let measureType = UILabel()
-
-  override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-    super.init(style: style, reuseIdentifier: reuseIdentifier)
-
-    measureType.translatesAutoresizingMaskIntoConstraints = false
-    measureType.font = UIFont.boldSystemFont(ofSize: 14)
-    contentView.addSubview(measureType)
-
-    let viewDict = ["measureType": measureType] as [String: Any]
-
-    contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-(<=1)-[measureType]", options: NSLayoutFormatOptions.alignAllCenterY, metrics: nil, views: viewDict))
-    contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-(<=1)-[measureType]", options: NSLayoutFormatOptions.alignAllCenterX, metrics: nil, views: viewDict))
   }
 
   required init?(coder aDecoder: NSCoder) {
