9b7a441d3a6b0af8cc0776a5946b48742a8e93b8
Guillaume Roux <groux@ekylibre.com>
2018-08-31 11:51:24 +0200

    Reorganised selectdate class: added marks, split into functions

--- Views/SelectDateView.swift
+++ Views/SelectDateView.swift
@@ -9,38 +9,52 @@
 import UIKit
 
 class SelectDateView: UIView {
-  var datePicker: UIDatePicker!
-  var validateButton: UIButton!
+
+  //MARK: - Properties
+
+  lazy var datePicker: UIDatePicker = {
+    let datePicker = UIDatePicker(frame: CGRect.zero)
+    datePicker.datePickerMode = .date
+    datePicker.locale = Locale(identifier: "fr")
+    datePicker.translatesAutoresizingMaskIntoConstraints = false
+    return datePicker
+  }()
+
+  lazy var validateButton: UIButton = {
+    let validateButton = UIButton(frame: CGRect.zero)
+    validateButton.backgroundColor = UIColor.white
+    validateButton.setTitle("VALIDER", for: .normal)
+    validateButton.setTitleColor(AppColor.TextColors.Black, for: .normal)
+    validateButton.translatesAutoresizingMaskIntoConstraints = false
+    return validateButton
+  }()
+
+  //MARK: - Initialization
 
   override init(frame: CGRect) {
     super.init(frame: frame)
+    setupView()
+  }
 
-    datePicker = UIDatePicker()
-    datePicker.datePickerMode = .date
-    datePicker.locale = Locale(identifier: "fr")
+  private func setupView() {
+    self.backgroundColor = UIColor.white
+    self.layer.cornerRadius = 3
+    self.clipsToBounds = true
+    self.isHidden = true
     self.addSubview(datePicker)
-    datePicker.translatesAutoresizingMaskIntoConstraints = false
+    self.addSubview(validateButton)
+    setupLayout()
+  }
+
+  private func setupLayout() {
     NSLayoutConstraint(item: datePicker, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 5).isActive = true
     NSLayoutConstraint(item: datePicker, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
     //NSLayoutConstraint(item: datePicker, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 175).isActive = true
 
-    validateButton = UIButton(frame: CGRect.zero)
-    validateButton.backgroundColor = UIColor.white
-    validateButton.setTitle("VALIDER", for: .normal)
-    validateButton.setTitleColor(AppColor.TextColors.Black, for: .normal)
-    self.addSubview(validateButton)
-    validateButton.translatesAutoresizingMaskIntoConstraints = false
     NSLayoutConstraint(item: validateButton, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: -10).isActive = true
     NSLayoutConstraint(item: validateButton, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: -10).isActive = true
     NSLayoutConstraint(item: validateButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 30).isActive = true
     NSLayoutConstraint(item: validateButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100).isActive = true
-    validateButton.layer.cornerRadius = 5
-    validateButton.clipsToBounds = true
-
-    self.backgroundColor = UIColor.white
-    self.layer.cornerRadius = 5
-    self.clipsToBounds = true
-    self.isHidden = true
   }
 
   required init?(coder aDecoder: NSCoder) {
