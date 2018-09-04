012fb513ba12530830e72668ec87b1db967672ac
TontonBuck <tontonbuck@live.fr>
2018-09-04 14:01:46 +0200

    Changed crops view layout: now using anchors

--- Views/CropsView.swift
+++ Views/CropsView.swift
@@ -103,27 +103,28 @@ class CropsView: UIView, UITableViewDataSource, UITableViewDelegate {
   }
 
   private func setupLayout() {
-    let viewsDict = [
-      "header" : headerView,
-      "title" : titleLabel,
-      "table" : tableView,
-      "bottom" : bottomView,
-      "selected" : selectedCropsLabel,
-      "validate" : validateButton,
-      ] as [String : Any]
-
-    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[title]", options: [], metrics: nil, views: viewsDict))
-    NSLayoutConstraint(item: titleLabel, attribute: .centerY, relatedBy: .equal, toItem: headerView, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
-
-    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-30-[selected]", options: [], metrics: nil, views: viewsDict))
-    NSLayoutConstraint(item: selectedCropsLabel, attribute: .centerY, relatedBy: .equal, toItem: bottomView, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
-    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[validate(>=100)]-13-|", options: [], metrics: nil, views: viewsDict))
-    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-13-[validate]-13-|", options: [], metrics: nil, views: viewsDict))
-
-    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[header]|", options: [], metrics: nil, views: viewsDict))
-    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[table]|", options: [], metrics: nil, views: viewsDict))
-    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[bottom]|", options: [], metrics: nil, views: viewsDict))
-    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[header(60)][table][bottom(60)]|", options: [], metrics: nil, views: viewsDict))
+    NSLayoutConstraint.activate([
+      titleLabel.centerYAnchor.constraint(equalTo: headerView.centerYAnchor),
+      titleLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 20),
+      headerView.topAnchor.constraint(equalTo: self.topAnchor),
+      headerView.heightAnchor.constraint(equalToConstant: 60),
+      headerView.leftAnchor.constraint(equalTo: self.leftAnchor),
+      headerView.widthAnchor.constraint(equalTo: self.widthAnchor),
+      tableView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
+      tableView.bottomAnchor.constraint(equalTo: bottomView.topAnchor),
+      tableView.leftAnchor.constraint(equalTo: self.leftAnchor),
+      tableView.widthAnchor.constraint(equalTo: self.widthAnchor),
+      selectedCropsLabel.centerYAnchor.constraint(equalTo: bottomView.centerYAnchor),
+      selectedCropsLabel.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor, constant: 30),
+      validateButton.topAnchor.constraint(equalTo: bottomView.topAnchor, constant: 13),
+      validateButton.bottomAnchor.constraint(equalTo: bottomView.bottomAnchor, constant: -13),
+      validateButton.trailingAnchor.constraint(equalTo: bottomView.trailingAnchor, constant: -13),
+      validateButton.widthAnchor.constraint(equalToConstant: 100),
+      bottomView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+      bottomView.heightAnchor.constraint(equalToConstant: 60),
+      bottomView.leftAnchor.constraint(equalTo: self.leftAnchor),
+      bottomView.widthAnchor.constraint(equalTo: self.widthAnchor),
+      ])
   }
 
   required init?(coder aDecoder: NSCoder) {
