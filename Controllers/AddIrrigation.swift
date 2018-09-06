5132371b4596b4267ae9599c9ca3a63da363d579
TontonBuck <tontonbuck@live.fr>
2018-09-06 11:48:49 +0200

    Added irrigation extension, which will contain all methods related to irrigation view

--- Controllers/AddIrrigation.swift
+++ Controllers/AddIrrigation.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2018 Ekylibre. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension AddInterventionViewController {
+
+  @IBAction func tapIrrigationView(_ sender: Any) {
+    if irrigationHeightConstraint.constant == 70 {
+      irrigationHeightConstraint.constant = 140
+      irrigationValueTextField.isHidden = false
+    } else {
+      irrigationHeightConstraint.constant = 70
+      irrigationValueTextField.isHidden = true
+    }
+    irrigationExpandCollapseImage.transform = irrigationExpandCollapseImage.transform.rotated(by: CGFloat.pi)
+  }
+}
