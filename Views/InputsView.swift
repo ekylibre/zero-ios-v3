3af9e2d5139307b30ca71b4b486e64330efa7f50
TontonBuck <tontonbuck@live.fr>
2018-09-10 16:01:03 +0200

    Fertilizer natures are now translated

--- Views/InputsView.swift
+++ Views/InputsView.swift
@@ -220,7 +220,8 @@ class InputsView: UIView, UITableViewDataSource, UITableViewDelegate, UISearchBa
       let fromFertilizers = isSearching ? filteredInputs : fertilizers
 
       cell.nameLabel.text = fromFertilizers[indexPath.row].value(forKey: "name") as? String
-      cell.natureLabel.text = fromFertilizers[indexPath.row].value(forKey: "nature") as? String
+      let nature = fromFertilizers[indexPath.row].value(forKey: "nature") as? String
+      cell.natureLabel.text = nature?.localized
       return cell
     default:
       fatalError("Switch error")
