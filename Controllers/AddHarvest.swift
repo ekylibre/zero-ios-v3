60dbc1aa7cafe42de0456101bd78b9a3009ebbb3
Jonathan De Haay <jdehaay@ekylibre.com>
2018-09-12 15:15:50 +0200

    Finish harvest view.
    Non functionnal yet.

---
+++ Controllers/AddHarvest.swift
@@ -0,0 +1,20 @@
+//
+//  AddHarvest.swift
+//  Clic&Farm-iOS
+//
+//  Created by Jonathan DE HAAY on 12/09/2018.
+//  Copyright © 2018 Ekylibre. All rights reserved.
+//
+
+import UIKit
+
+extension AddInterventionViewController {
+  func initializeHarvestTableView() {
+    harvestTableView.layer.borderWidth  = 0.5
+    harvestTableView.layer.borderColor = UIColor.lightGray.cgColor
+    harvestTableView.layer.cornerRadius = 4
+    harvestTableView.dataSource = self
+    harvestTableView.delegate = self
+    harvestTableView.bounces = false
+  }
+}
---
+++ Views/Cells/HarvestCell.swift
@@ -0,0 +1,17 @@
+//
+//  HarvestCell.swift
+//  Clic&Farm-iOS
+//
+//  Created by Jonathan DE HAAY on 12/09/2018.
+//  Copyright © 2018 Ekylibre. All rights reserved.
+//
+
+import UIKit
+
+class HarvestCell: UITableViewCell {
+  @IBOutlet weak var number: UITextField!
+  @IBOutlet weak var quantity: UITextField!
+  @IBOutlet weak var delete: UIButton!
+  @IBOutlet weak var storage: UIButton!
+  @IBOutlet weak var unit: UIButton!
+}
