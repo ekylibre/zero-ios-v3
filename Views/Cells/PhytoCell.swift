57f9bbfc1409d86b9647039038f4e3e9f9464c4b
Jonathan De Haay <jdehaay@ekylibre.com>
2018-09-24 16:10:46 +0200

    Pull from master and update to swift 4.2

--- Views/Cells/PhytoCell.swift
+++ Views/Cells/PhytoCell.swift
@@ -57,7 +57,7 @@ class PhytoCell: UITableViewCell {
     return inFieldReentryDelayLabel
   }()
 
-  override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
     setupCell()
   }
