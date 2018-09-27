61c052aa5984681c3fe9359de42ceb3aad7fbd96
TontonBuck <tontonbuck@live.fr>
2018-09-27 16:02:53 +0200

    Fixed wrong var name

--- Controllers/AddIrrigation.swift
+++ Controllers/AddIrrigation.swift
@@ -53,12 +53,12 @@ extension AddInterventionViewController: UITextFieldDelegate, CustomPickerViewPr
   // MARK: - Actions
 
   @IBAction func tapIrrigationView(_ sender: Any) {
-    let shouldExapand: Bool = (irrigationHeightConstraint.constant == 70)
+    let shouldExpand: Bool = (irrigationHeightConstraint.constant == 70)
 
-    irrigationHeightConstraint.constant = shouldExapand ? 140 : 70
-    irrigationLabel.isHidden = shouldExapand
-    irrigationValueTextField.isHidden = !shouldExapand
-    irrigationUnitButton.isHidden = !shouldExapand
+    irrigationHeightConstraint.constant = shouldExpand ? 140 : 70
+    irrigationLabel.isHidden = shouldExpand
+    irrigationValueTextField.isHidden = !shouldExpand
+    irrigationUnitButton.isHidden = !shouldExpand
     irrigationExpandCollapseImage.transform = irrigationExpandCollapseImage.transform.rotated(by: CGFloat.pi)
   }
 
