189e355e2845933d3521ada4209c48797abba73f
Jonathan De Haay <jdehaay@ekylibre.com>
2018-10-05 11:44:39 +0200

    Change equipment types defining method

--- Controllers/AddEquipmentIntervention.swift
+++ Controllers/AddEquipmentIntervention.swift
@@ -45,94 +45,9 @@ extension AddInterventionViewController: SelectedEquipmentCellDelegate {
   }
 
   func defineEquipmentImage(equipmentName: String) -> UIImage? {
-    let equipmentTypes = loadEquipmentTypes()
+    let assetName = equipmentName.lowercased().replacingOccurrences(of: "_", with: "-")
 
-    switch equipmentName.uppercased() {
-    case equipmentTypes[0]:
-      return #imageLiteral(resourceName: "airplanter")
-    case equipmentTypes[1]:
-      return #imageLiteral(resourceName: "baler-wrapper")
-    case equipmentTypes[2]:
-      return #imageLiteral(resourceName: "corn-topper")
-    case equipmentTypes[3]:
-      return #imageLiteral(resourceName: "cubic-baler")
-    case equipmentTypes[4]:
-      return #imageLiteral(resourceName: "disc-harrow")
-    case equipmentTypes[5]:
-      return #imageLiteral(resourceName: "forage-platform")
-    case equipmentTypes[6]:
-      return #imageLiteral(resourceName: "forager")
-    case equipmentTypes[7]:
-      return #imageLiteral(resourceName: "grinder")
-    case equipmentTypes[8]:
-      return #imageLiteral(resourceName: "harrow")
-    case equipmentTypes[9]:
-      return #imageLiteral(resourceName: "harvester")
-    case equipmentTypes[10]:
-      return #imageLiteral(resourceName: "hay-rake")
-    case equipmentTypes[11]:
-      return #imageLiteral(resourceName: "hiller")
-    case equipmentTypes[12]:
-      return #imageLiteral(resourceName: "hoe")
-    case equipmentTypes[13]:
-      return #imageLiteral(resourceName: "hoe_weeder")
-    case equipmentTypes[14]:
-      return #imageLiteral(resourceName: "implanter")
-    case equipmentTypes[15]:
-      return #imageLiteral(resourceName: "irrigation-pivot")
-    case equipmentTypes[16]:
-      return #imageLiteral(resourceName: "liquid-manure-spreader")
-    case equipmentTypes[17]:
-      return #imageLiteral(resourceName: "mower")
-    case equipmentTypes[18]:
-      return #imageLiteral(resourceName: "mower-conditioner")
-    case equipmentTypes[19]:
-      return #imageLiteral(resourceName: "plow")
-    case equipmentTypes[20]:
-      return #imageLiteral(resourceName: "reaper")
-    case equipmentTypes[21]:
-      return #imageLiteral(resourceName: "roll")
-    case equipmentTypes[22]:
-      return #imageLiteral(resourceName: "rotary-hoe")
-    case equipmentTypes[23]:
-      return #imageLiteral(resourceName: "round-baler")
-    case equipmentTypes[24]:
-      return #imageLiteral(resourceName: "seedbed-preparator")
-    case equipmentTypes[25]:
-      return #imageLiteral(resourceName: "soil-loosener")
-    case equipmentTypes[26]:
-      return #imageLiteral(resourceName: "sower")
-    case equipmentTypes[27]:
-      return #imageLiteral(resourceName: "sprayer")
-    case equipmentTypes[28]:
-      return #imageLiteral(resourceName: "spreader")
-    case equipmentTypes[29]:
-      return #imageLiteral(resourceName: "spreader-trailer")
-    case equipmentTypes[30]:
-      return #imageLiteral(resourceName: "subsoil-plow")
-    case equipmentTypes[31]:
-      return #imageLiteral(resourceName: "superficial-plow")
-    case equipmentTypes[32]:
-      return #imageLiteral(resourceName: "tedder")
-    case equipmentTypes[33]:
-      return #imageLiteral(resourceName: "topper")
-    case equipmentTypes[34]:
-      return #imageLiteral(resourceName: "tractor")
-    case equipmentTypes[35]:
-      return #imageLiteral(resourceName: "trailer")
-    case equipmentTypes[36]:
-      return #imageLiteral(resourceName: "trimmer")
-    case equipmentTypes[37]:
-      return #imageLiteral(resourceName: "vibrocultivator")
-    case equipmentTypes[38]:
-      return #imageLiteral(resourceName: "water-spreader")
-    case equipmentTypes[39]:
-      return #imageLiteral(resourceName: "weeder")
-    case equipmentTypes[40]:
-      return #imageLiteral(resourceName: "wrapper")
-    default:
-      return nil
-    }
+    return UIImage(named: assetName)
   }
 
   // MARK: - Actions
