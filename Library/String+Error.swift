2482badff0240b04e0721ac2a248653c4baa4aa5
TontonBuck <tontonbuck@live.fr>
2018-11-08 14:26:07 +0100

    Added String extension to throw errors using a string

---
+++ Library/String+Error.swift
@@ -0,0 +1,9 @@
+//
+//  String+Error.swift
+//  Clic&Farm-iOS
+//
+//  Created by Guillaume Roux on 08/11/2018.
+//  Copyright © 2018 Ekylibre. All rights reserved.
+//
+
+extension String: Error {}
