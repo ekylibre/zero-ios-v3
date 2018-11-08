b791caeeb3c2eb8a49f41c797bc8d1e32eb575d0
TontonBuck <tontonbuck@live.fr>
2018-11-08 11:28:11 +0100

    Commented shape in Farms query, removed workZone from schema, removed Polygon from CustomTypes. We are not using Polygon custom type yet because of inheritance problems with MKPolygon and MGLPolygon classes

--- GraphQL/CustomTypes.swift
+++ GraphQL/CustomTypes.swift
@@ -34,7 +34,6 @@ extension Date: JSONDecodable, JSONEncodable {
 }
 
 public typealias Point = MKMapPoint
-public typealias Polygon = MKPolygon
 
 extension Point: JSONDecodable, JSONEncodable {
   public init(jsonValue value: JSONValue) throws {
@@ -54,32 +53,3 @@ extension Point: JSONDecodable, JSONEncodable {
     return String(format: "[%@,%@]", String(self.x), String(self.y))
   }
 }
-
-extension MKPolygon: JSONDecodable {
-  init(jsonValue value: JSONValue) throws {
-    guard let string = value as? String else {
-      throw JSONDecodingError.couldNotConvert(value: value, to: String.self)
-    }
-
-    let points = string[3 ... string.count - 3].split(separator: ",")
-    var lngLats = [Point]()
-
-    for point in points {
-      let lngLat = point.split(separator: ",")
-      lngLats.append(Point(x: Double(lngLat[1])!, y: Double(lngLat[0])!))
-    }
-    return Polygon(points: lngLats, count: lngLats.count)
-  }
-}
-
-  /*public var jsonValue: JSONValue {
-    let points = self.points()
-    let string = "["
-
-    for case let point as Point in points {
-      string.append("[")
-      string.appendingFormat("[%s,%s]", point.x, point.y)
-    }
-    string.append("]")
-  }
-}*/
