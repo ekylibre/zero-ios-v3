89cb920410a8e6847ffc8c1ad279b56601e1a8cf
TontonBuck <tontonbuck@live.fr>
2018-11-14 15:10:35 +0100

    Addign wokringPeriods tests in order to test label updating when expanding view. Edited tapWorkingPeriod method

--- Clic&Farm-iOSTests/WorkingPeriodTests.swift
+++ Clic&Farm-iOSTests/WorkingPeriodTests.swift
@@ -25,6 +25,118 @@ class WorkingPeriodTests: XCTestCase {
     super.tearDown()
   }
 
+  func test_selectedWorkingPeriodLabel_withDefaultValues_shouldNotChange() {
+    //Given
+    let expectedDate: String = {
+      let dateFormatter = DateFormatter()
+      dateFormatter.locale = Locale(identifier: "locale".localized)
+      dateFormatter.dateFormat = "d MMM"
+      return dateFormatter.string(from: Date())
+    }()
+
+    //When
+    var expectedString = String(format: "%@ • 7 h", "today".localized.lowercased())
+    XCTAssertEqual(addInterventionVC.selectedWorkingPeriodLabel.text, expectedString,
+                   "SelectedWorkingPeriodLabel text should be this one before expanding the view")
+    addInterventionVC.tapWorkingPeriodView(self)
+    addInterventionVC.tapWorkingPeriodView(self)
+
+    //Then
+    expectedString = String(format: "%@ • 7 h", expectedDate)
+    XCTAssertEqual(addInterventionVC.selectedWorkingPeriodLabel.text, expectedString,
+                   "Today word must be replaced by today's date with current format: 'd MMM'")
+  }
+
+  func test_selectedWorkingPeriodLabel_withDurationChanged_shouldChange() {
+    //Given
+    let duration = "0,10"
+    let expectedDate: String = {
+      let dateFormatter = DateFormatter()
+      dateFormatter.locale = Locale(identifier: "locale".localized)
+      dateFormatter.dateFormat = "d MMM"
+      return dateFormatter.string(from: Date())
+    }()
+
+    //When
+    var expectedString = String(format: "%@ • 7 h", "today".localized.lowercased())
+    XCTAssertEqual(addInterventionVC.selectedWorkingPeriodLabel.text, expectedString,
+                   "SelectedWorkingPeriodLabel text should be this one before expanding the view")
+    addInterventionVC.workingPeriodDurationTextField.text = duration
+    addInterventionVC.tapWorkingPeriodView(self)
+    addInterventionVC.tapWorkingPeriodView(self)
+
+    //Then
+    expectedString = String(format: "%@ • 0.1 h", expectedDate)
+    XCTAssertEqual(addInterventionVC.selectedWorkingPeriodLabel.text, expectedString,
+                   "Label must contain new duration value")
+  }
+
+  func test_selectedWorkingPeriodLabel_withDateChanged_shouldChange() {
+    //Given
+    let date: Date = {
+      let calendar = Calendar(identifier: .gregorian)
+      var dateComponents = DateComponents()
+      dateComponents.year = 2010
+      dateComponents.month = 6
+      dateComponents.day = 29
+      guard let date = calendar.date(from: dateComponents) else { XCTFail("Date initialization error"); return Date() }
+      return date
+    }()
+    let dateString: String = {
+      let dateFormatter = DateFormatter()
+      dateFormatter.locale = Locale(identifier: "locale".localized)
+      dateFormatter.dateFormat = "d MMM"
+      return dateFormatter.string(from: date)
+    }()
+
+    //When
+    var expectedString = String(format: "%@ • 7 h", "today".localized.lowercased())
+    XCTAssertEqual(addInterventionVC.selectedWorkingPeriodLabel.text, expectedString,
+                   "SelectedWorkingPeriodLabel text should be this one before expanding the view")
+    addInterventionVC.workingPeriodDateButton.setTitle(dateString, for: .normal)
+    addInterventionVC.tapWorkingPeriodView(self)
+    addInterventionVC.tapWorkingPeriodView(self)
+
+    //Then
+    expectedString = String(format: "%@ • 7 h", dateString)
+    XCTAssertEqual(addInterventionVC.selectedWorkingPeriodLabel.text, expectedString,
+                   "Label must contain new date value")
+  }
+
+  func test_selectedWorkingPeriodLabel_withDateAndDurationChanged_shouldChange() {
+    //Given
+    let date: Date = {
+      let calendar = Calendar(identifier: .gregorian)
+      var dateComponents = DateComponents()
+      dateComponents.year = 2010
+      dateComponents.month = 6
+      dateComponents.day = 29
+      guard let date = calendar.date(from: dateComponents) else { XCTFail("Date initialization error"); return Date() }
+      return date
+    }()
+    let dateString: String = {
+      let dateFormatter = DateFormatter()
+      dateFormatter.locale = Locale(identifier: "locale".localized)
+      dateFormatter.dateFormat = "d MMM"
+      return dateFormatter.string(from: date)
+    }()
+    let duration = "3.052"
+
+    //When
+    var expectedString = String(format: "%@ • 7 h", "today".localized.lowercased())
+    XCTAssertEqual(addInterventionVC.selectedWorkingPeriodLabel.text, expectedString,
+                   "SelectedWorkingPeriodLabel text should be this one before expanding the view")
+    addInterventionVC.workingPeriodDurationTextField.text = duration
+    addInterventionVC.workingPeriodDateButton.setTitle(dateString, for: .normal)
+    addInterventionVC.tapWorkingPeriodView(self)
+    addInterventionVC.tapWorkingPeriodView(self)
+
+    //Then
+    expectedString = String(format: "%@ • 3.1 h", dateString)
+    XCTAssertEqual(addInterventionVC.selectedWorkingPeriodLabel.text, expectedString,
+                   "Label must contain new date and duration values")
+  }
+
   func test_unitLabel_withDurationLesserThanOrEqualTo1_shouldBeSingular() {
     //Given
     let duration = "1.00"
