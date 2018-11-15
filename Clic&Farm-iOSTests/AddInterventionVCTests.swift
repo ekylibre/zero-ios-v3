9b62b2a1588dd0e88094b5151bd2aff72bb15330
Jonathan De Haay <jdehaay@ekylibre.com>
2018-11-15 12:07:50 +0100

    Add tests for tableViewNumberOfRowInSection in addInterventionVC

--- Clic&Farm-iOSTests/AddInterventionVCTests.swift
+++ Clic&Farm-iOSTests/AddInterventionVCTests.swift
@@ -90,4 +90,80 @@ class AddInterventionVCTests: XCTestCase {
     XCTAssertFalse(addInterventionVC.irrigationView.isHidden, "Irrigation view should be displayed")
     XCTAssertFalse(addInterventionVC.irrigationSeparatorView.isHidden, "Irrigation separator view should be displayed")
   }
-}
+
+  func test_tableViewNumberOfRowsInSection_withSelectedInputsTableViewAndNoData_shouldReturnZero() {
+    //When
+    addInterventionVC.selectedInputs.removeAll()
+    addInterventionVC.selectedInputsTableView.reloadData()
+
+    //Then
+    let numberOfRowsInSection = addInterventionVC.tableView(
+      addInterventionVC.selectedInputsTableView,
+      numberOfRowsInSection: addInterventionVC.selectedInputs.count)
+
+    XCTAssertEqual(numberOfRowsInSection, 0, "Should have no inputs")
+  }
+
+  func test_tableViewNumberOfRowsInSection_withSelectedMaterialsTableViewAndNoData_shouldReturnZero() {
+    //When
+    addInterventionVC.selectedMaterials[0].removeAll()
+    addInterventionVC.selectedMaterialsTableView.reloadData()
+
+    //Then
+    let numberOfRowsInSection = addInterventionVC.tableView(
+      addInterventionVC.selectedMaterialsTableView,
+      numberOfRowsInSection: addInterventionVC.selectedMaterials[0].count)
+
+    XCTAssertEqual(numberOfRowsInSection, 0, "Should have no materials")
+  }
+
+  func test_tableViewNumberOfRowsInSection_withSelectedEquipmentsTableViewAndNoData_shouldReturnZero() {
+    //When
+    addInterventionVC.selectedEquipments.removeAll()
+    addInterventionVC.selectedEquipmentsTableView.reloadData()
+
+    //Then
+    let numberOfRowsInSection = addInterventionVC.tableView(
+      addInterventionVC.selectedEquipmentsTableView,
+      numberOfRowsInSection: addInterventionVC.selectedEquipments.count)
+
+    XCTAssertEqual(numberOfRowsInSection, 0, "Should have no equipments")
+  }
+
+  func test_tableViewNumberOfRowsInSection_withSelectedPersonsTableViewAndNoData_shouldReturnZero() {
+    //When
+    addInterventionVC.selectedPersons[0].removeAll()
+    addInterventionVC.selectedPersonsTableView.reloadData()
+
+    //Then
+    let numberOfRowsInSection = addInterventionVC.tableView(
+      addInterventionVC.selectedPersonsTableView,
+      numberOfRowsInSection: addInterventionVC.selectedPersons[0].count)
+
+    XCTAssertEqual(numberOfRowsInSection, 0, "Should have no persons")
+  }
+
+  func test_tableViewNumberOfRowsInSection_withHarvestTableViewAndNoData_shouldReturnZero() {
+    //When
+    addInterventionVC.harvests.removeAll()
+    addInterventionVC.harvestTableView.reloadData()
+
+    //Then
+    let numberOfRowsInSection = addInterventionVC.tableView(
+      addInterventionVC.harvestTableView,
+      numberOfRowsInSection: addInterventionVC.harvests.count)
+
+    XCTAssertEqual(numberOfRowsInSection, 0, "Should have no harvests")
+  }
+
+  func test_tableViewNumberOfRowsInSection_withRandomTableViewAndNoData_shouldReturnOne() {
+    //Given
+    let tableView = UITableView()
+
+    //Then
+    let numberOfRowsInSection = addInterventionVC.tableView(
+      tableView,
+      numberOfRowsInSection: 0)
+
+    XCTAssertEqual(numberOfRowsInSection, 1, "Should return one")
+  }}
