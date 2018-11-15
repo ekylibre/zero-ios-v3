b01c759c1a22882cb78a377be092ed7f37d728c6
TontonBuck <tontonbuck@live.fr>
2018-11-15 15:51:20 +0100

    Added materials and equipments tests. Testing creation with already existing names

--- Clic&Farm-iOSTests/EquipmentsTests.swift
+++ Clic&Farm-iOSTests/EquipmentsTests.swift
@@ -7,21 +7,44 @@
 //
 
 import XCTest
+import CoreData
 @testable import Clic_Farm_iOS
 
 class EquipmentsTests: XCTestCase {
 
   var addInterventionVC: AddInterventionViewController!
+  var sut: StorageManager!
+  let managedObjectModel: NSManagedObjectModel = {
+    let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle.main])!
+    return managedObjectModel
+  }()
+  lazy var mockPersistentContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: "MockedContainer", managedObjectModel: self.managedObjectModel)
+    let description = NSPersistentStoreDescription()
+    description.type = NSInMemoryStoreType
+    description.shouldAddStoreAsynchronously = false
+    container.persistentStoreDescriptions = [description]
+    container.loadPersistentStores { (description, error) in
+      precondition(description.type == NSInMemoryStoreType)
+
+      if let error = error {
+        fatalError("Create an in-mem coordinator failed \(error)")
+      }
+    }
+    return container
+  }()
 
   override func setUp() {
     super.setUp()
     let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
     addInterventionVC = storyboard.instantiateViewController(withIdentifier: "AddInterventionVC") as? AddInterventionViewController
     let _ = addInterventionVC.view
+    sut = StorageManager(container: mockPersistentContainer)
   }
 
   override func tearDown() {
     addInterventionVC = nil
+    sut = nil
     super.tearDown()
   }
 
@@ -54,15 +77,48 @@ class EquipmentsTests: XCTestCase {
     XCTAssertFalse(addInterventionVC.equipmentsSelectionView.checkEquipmentName(),
                   "checkEquipmentName must return false when the name is empty")
     XCTAssertFalse(addInterventionVC.equipmentsSelectionView.creationView.errorLabel.isHidden,
-                   "errorLabel must be hidden when there is an error")
+                   "errorLabel must not be hidden when there is an error")
   }
 
-  func test_updateEquipmentsCountLabelWithoutEquipment_shouldNotChange() {
+  func test_checkEquipmentName_withUniqueName_shouldReturnTrue() {
+    //Given
+    let equipment = sut.insertObject(entityName: "Equipment") as! Equipment
+    let name = "Sample"
+
     //When
-    addInterventionVC.selectedEquipments.removeAll()
-    addInterventionVC.updateEquipmentsCountLabel()
+    addInterventionVC.equipmentsSelectionView.equipments.removeAll()
+    XCTAssertEqual(addInterventionVC.equipmentsSelectionView.equipments.count, 0, "equipments must be empty")
+    equipment.name = "Sample equipment"
+    addInterventionVC.equipmentsSelectionView.equipments.append(equipment)
+    XCTAssertEqual(addInterventionVC.equipmentsSelectionView.equipments.count, 1,
+                   "equipments must contain newly created equipment")
+    addInterventionVC.equipmentsSelectionView.creationView.nameTextField.text = name
 
     //Then
-    XCTAssertEqual(addInterventionVC.equipmentsCountLabel.text, "none".localized, "Should have no equipments")
+    XCTAssertTrue(addInterventionVC.equipmentsSelectionView.checkEquipmentName(),
+                  "checkEquipmentName must return true when there is not an existing equipment with this same name")
+    XCTAssertTrue(addInterventionVC.equipmentsSelectionView.creationView.errorLabel.isHidden,
+                   "errorLabel must be hidden when there is not any error")
+  }
+
+  func test_checkEquipmentName_withExistingName_shouldReturnFalse() {
+    //Given
+    let equipment = sut.insertObject(entityName: "Equipment") as! Equipment
+    let name = "Sample equipment"
+
+    //When
+    addInterventionVC.equipmentsSelectionView.equipments.removeAll()
+    XCTAssertEqual(addInterventionVC.equipmentsSelectionView.equipments.count, 0, "equipments must be empty")
+    equipment.name = "Sample equipment"
+    addInterventionVC.equipmentsSelectionView.equipments.append(equipment)
+    XCTAssertEqual(addInterventionVC.equipmentsSelectionView.equipments.count, 1,
+                   "equipments must contain newly created equipment")
+    addInterventionVC.equipmentsSelectionView.creationView.nameTextField.text = name
+
+    //Then
+    XCTAssertFalse(addInterventionVC.equipmentsSelectionView.checkEquipmentName(),
+                  "checkEquipmentName must return false when the name is already taken by an equipment")
+    XCTAssertFalse(addInterventionVC.equipmentsSelectionView.creationView.errorLabel.isHidden,
+                   "errorLabel must not be hidden when there is an error")
   }
 }
--- Clic&Farm-iOSTests/MaterialsTests.swift
+++ Clic&Farm-iOSTests/MaterialsTests.swift
@@ -7,21 +7,44 @@
 //
 
 import XCTest
+import CoreData
 @testable import Clic_Farm_iOS
 
 class MaterialsTests: XCTestCase {
 
   var addInterventionVC: AddInterventionViewController!
+  var sut: StorageManager!
+  let managedObjectModel: NSManagedObjectModel = {
+    let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle.main])!
+    return managedObjectModel
+  }()
+  lazy var mockPersistentContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: "MockedContainer", managedObjectModel: self.managedObjectModel)
+    let description = NSPersistentStoreDescription()
+    description.type = NSInMemoryStoreType
+    description.shouldAddStoreAsynchronously = false
+    container.persistentStoreDescriptions = [description]
+    container.loadPersistentStores { (description, error) in
+      precondition(description.type == NSInMemoryStoreType)
+
+      if let error = error {
+        fatalError("Create an in-mem coordinator failed \(error)")
+      }
+    }
+    return container
+  }()
 
   override func setUp() {
     super.setUp()
     let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
     addInterventionVC = storyboard.instantiateViewController(withIdentifier: "AddInterventionVC") as? AddInterventionViewController
     let _ = addInterventionVC.view
+    sut = StorageManager(container: mockPersistentContainer)
   }
 
   override func tearDown() {
     addInterventionVC = nil
+    sut = nil
     super.tearDown()
   }
 
@@ -55,5 +78,47 @@ class MaterialsTests: XCTestCase {
                   "checkMaterialName must return true when the name is empty")
     XCTAssertFalse(addInterventionVC.materialsSelectionView.creationView.errorLabel.isHidden,
                    "errorLabel must not be hidden when there is an error")
+  }
+
+  func test_checkMaterialName_withUniqueName_shouldReturnTrue() {
+    //Given
+    let material = sut.insertObject(entityName: "Material") as! Material
+    let name = "Sample"
+
+    //When
+    addInterventionVC.materialsSelectionView.materials.removeAll()
+    XCTAssertEqual(addInterventionVC.materialsSelectionView.materials.count, 0, "materials must be empty")
+    material.name = "Sample material"
+    addInterventionVC.materialsSelectionView.materials.append(material)
+    XCTAssertEqual(addInterventionVC.materialsSelectionView.materials.count, 1,
+                   "materials must contain newly created material")
+    addInterventionVC.materialsSelectionView.creationView.nameTextField.text = name
+
+    //Then
+    XCTAssertTrue(addInterventionVC.materialsSelectionView.checkMaterialName(),
+                  "checkMaterialName must return true when there is not an existing material with this same name")
+    XCTAssertTrue(addInterventionVC.materialsSelectionView.creationView.errorLabel.isHidden,
+                  "errorLabel must be hidden when there is not any error")
+  }
+
+  func test_checkMaterialName_withExistingName_shouldReturnFalse() {
+    //Given
+    let material = sut.insertObject(entityName: "Material") as! Material
+    let name = "Sample material"
+
+    //When
+    addInterventionVC.materialsSelectionView.materials.removeAll()
+    XCTAssertEqual(addInterventionVC.materialsSelectionView.materials.count, 0, "materials must be empty")
+    material.name = "Sample material"
+    addInterventionVC.materialsSelectionView.materials.append(material)
+    XCTAssertEqual(addInterventionVC.materialsSelectionView.materials.count, 1,
+                   "materials must contain newly created material")
+    addInterventionVC.materialsSelectionView.creationView.nameTextField.text = name
+
+    //Then
+    XCTAssertFalse(addInterventionVC.materialsSelectionView.checkMaterialName(),
+                  "checkMaterialName must return false when the name is already taken by a material")
+    XCTAssertFalse(addInterventionVC.materialsSelectionView.creationView.errorLabel.isHidden,
+                  "errorLabel must not be hidden when there is an error")
   }
 }
