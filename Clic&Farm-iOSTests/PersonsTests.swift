a2c9e54ffe656957a5d074e8f8127fa91be98f0b
TontonBuck <tontonbuck@live.fr>
2018-11-15 10:22:40 +0100

    Edited checkPersonName tests

--- Clic&Farm-iOSTests/PersonsTests.swift
+++ Clic&Farm-iOSTests/PersonsTests.swift
@@ -25,54 +25,83 @@ class PersonsTests: XCTestCase {
     super.tearDown()
   }
 
-  func test_checkPeronsName_withoutExistingPersonsAndAFirstNameAndALastName_shouldReturnTrue() {
+  func test_checkPeronsName_withValidNames_shouldReturnTrue() {
     //Given
-    let firstName = "Sample fist name"
+    let firstName = "Sample first name"
     let lastName = "Sample last name"
 
     //When
     addInterventionVC.personsSelectionView.persons.removeAll()
+    XCTAssertEqual(addInterventionVC.personsSelectionView.persons.count, 0, "persons must be empty")
     addInterventionVC.personsSelectionView.creationView.firstNameTextField.text = firstName
     addInterventionVC.personsSelectionView.creationView.lastNameTextField.text = lastName
 
     //Then
-    XCTAssertEqual(addInterventionVC.personsSelectionView.checkPersonName(), true,
-                   "Should return true")
+    XCTAssertTrue(addInterventionVC.personsSelectionView.checkPersonName(),
+                  "checkPersonName must return true when firstName and lastName are valid")
+    XCTAssertTrue(addInterventionVC.personsSelectionView.creationView.firstNameErrorLabel.isHidden,
+                  "firstNameErrorLabel must be hidden when there is not any error")
+    XCTAssertTrue(addInterventionVC.personsSelectionView.creationView.lastNameErrorLabel.isHidden,
+                  "lastNameErrorLabel must be hidden when there is not any error")
   }
 
-  func test_checkPeronsName_withoutExistingPersonsAndAFirstName_shouldReturnFalse() {
+  func test_checkPeronsName_withEmptyFirstName_shouldReturnFalse() {
     //Given
-    let firstName = "Sample fist name"
+    let firstName = ""
+    let lastName = "Sample last name"
 
     //When
     addInterventionVC.personsSelectionView.persons.removeAll()
+    XCTAssertEqual(addInterventionVC.personsSelectionView.persons.count, 0, "persons must be empty")
     addInterventionVC.personsSelectionView.creationView.firstNameTextField.text = firstName
+    addInterventionVC.personsSelectionView.creationView.lastNameTextField.text = lastName
 
     //Then
-    XCTAssertEqual(addInterventionVC.personsSelectionView.checkPersonName(), false,
-                   "Should return false")
+    XCTAssertFalse(addInterventionVC.personsSelectionView.checkPersonName(),
+                   "checkPersonName must return false when fistName is empty")
+    XCTAssertFalse(addInterventionVC.personsSelectionView.creationView.firstNameErrorLabel.isHidden,
+                   "firstNameErrorLabel must not be hidden when firstName is empty")
+    XCTAssertTrue(addInterventionVC.personsSelectionView.creationView.lastNameErrorLabel.isHidden,
+                  "lastNameErrorLabel must be hidden when lastName is not empty")
   }
 
   func test_checkPeronsName_withoutExistingPersonsAndALastName_shouldReturnFalse() {
     //Given
-    let lastName = "Sample last name"
+    let firstName = "Sample first name"
+    let lastName = ""
 
     //When
     addInterventionVC.personsSelectionView.persons.removeAll()
+    XCTAssertEqual(addInterventionVC.personsSelectionView.persons.count, 0, "persons must be empty")
+    addInterventionVC.personsSelectionView.creationView.firstNameTextField.text = firstName
     addInterventionVC.personsSelectionView.creationView.lastNameTextField.text = lastName
 
     //Then
     XCTAssertEqual(addInterventionVC.personsSelectionView.checkPersonName(), false,
-                   "Should return false")
+                   "checkPersonName must return false when lastName is empty")
+    XCTAssertTrue(addInterventionVC.personsSelectionView.creationView.firstNameErrorLabel.isHidden,
+                  "firstNameErrorLabel must be hidden when firstName is not empty")
+    XCTAssertFalse(addInterventionVC.personsSelectionView.creationView.lastNameErrorLabel.isHidden,
+                   "lastNameErrorLabel must not be hidden when lastName is empty")
   }
 
-  func test_checkPeronsName_withoutExistingPersonsAndNoName_shouldReturnFalse() {
+  func test_checkPeronsName_withEmptyNames_shouldReturnFalse() {
+    //Given
+    let firstName = ""
+    let lastName = ""
+
     //When
     addInterventionVC.personsSelectionView.persons.removeAll()
-    addInterventionVC.personsSelectionView.creationView.firstNameTextField.text = nil
+    XCTAssertEqual(addInterventionVC.personsSelectionView.persons.count, 0, "persons must be empty")
+    addInterventionVC.personsSelectionView.creationView.firstNameTextField.text = firstName
+    addInterventionVC.personsSelectionView.creationView.lastNameTextField.text = lastName
 
     //Then
-    XCTAssertEqual(addInterventionVC.personsSelectionView.checkPersonName(), false,
-                   "Should return false")
+    XCTAssertFalse(addInterventionVC.personsSelectionView.checkPersonName(),
+                   "checkPersonName must return false when firstName and lastName are empty")
+    XCTAssertFalse(addInterventionVC.personsSelectionView.creationView.firstNameErrorLabel.isHidden,
+                   "firstNameErrorLabel must not be hidden when firstName is empty")
+    XCTAssertFalse(addInterventionVC.personsSelectionView.creationView.lastNameErrorLabel.isHidden,
+                   "lastNameErrorLabel must not be hidden when lastName is empty")
   }
 }
