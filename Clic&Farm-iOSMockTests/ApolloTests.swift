33fca6b7584fc9fc2ee5401eebdc042261709fe3
TontonBuck <tontonbuck@live.fr>
2018-11-16 15:34:16 +0100

    Edited ApolloTests indentation

--- Clic&Farm-iOSMockTests/ApolloTests.swift
+++ Clic&Farm-iOSMockTests/ApolloTests.swift
@@ -12,16 +12,16 @@ import Apollo
 
 class ApolloTests: XCTestCase {
 
-    override func setUp() {
-      super.setUp()
-    }
+  override func setUp() {
+    super.setUp()
+  }
 
-    override func tearDown() {
-      super.tearDown()
-    }
+  override func tearDown() {
+    super.tearDown()
+  }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
 }
