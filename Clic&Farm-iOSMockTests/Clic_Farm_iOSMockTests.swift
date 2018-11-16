a42328f8b094eee12313db35c1217409007f3db9
TontonBuck <tontonbuck@live.fr>
2018-11-16 13:47:11 +0100

    Added MockTests target

---
+++ Clic&Farm-iOSMockTests/Clic_Farm_iOSMockTests.swift
@@ -0,0 +1,27 @@
+//
+//  Clic_Farm_iOSMockTests.swift
+//  Clic&Farm-iOSMockTests
+//
+//  Created by Guillaume Roux on 16/11/2018.
+//  Copyright © 2018 Ekylibre. All rights reserved.
+//
+
+import XCTest
+@testable import Clic_Farm_iOS
+import Apollo
+
+class Clic_Farm_iOSMockTests: XCTestCase {
+
+    override func setUp() {
+      super.setUp()
+    }
+
+    override func tearDown() {
+      super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
