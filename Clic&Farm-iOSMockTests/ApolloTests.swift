82d80ff3232177cd1aa8a1456062b2c1d0ef4dcd
TontonBuck <tontonbuck@live.fr>
2018-11-26 09:58:02 +0100

    Added mockTests target and ApolloTests class

---
+++ Clic&Farm-iOSMockTests/ApolloTests.swift
@@ -0,0 +1,41 @@
+//
+//  ApolloTests.swift
+//  Clic&Farm-iOSMockTests
+//
+//  Created by Guillaume Roux on 26/11/2018.
+//  Copyright © 2018 Ekylibre. All rights reserved.
+//
+
+import XCTest
+@testable import Clic_Farm_iOS
+import Apollo
+
+class ApolloTests: XCTestCase {
+
+  var client: ApolloClient!
+  let networkTransport: HTTPNetworkTransport = {
+    let url = URL(string: "https://api.ekylibre-test.com/v1/graphql")!
+    let configuation = URLSessionConfiguration.default
+    let authService = AuthentificationService(username: "", password: "")
+    let token = authService.oauth2.accessToken!
+    configuation.httpAdditionalHeaders = ["Authorization": "Bearer \(token)"]
+    return HTTPNetworkTransport(url: url, configuration: configuation)
+  }()
+  let store = ApolloStore(cache: InMemoryNormalizedCache(records: [
+    "QUERY_ROOT": ["hero": Reference(key: "hero")],
+    "hero": ["__typename": "Droid", "name": "R2-D2"]
+    ]))
+
+  override func setUp() {
+    client = ApolloClient(networkTransport: networkTransport, store: store)
+  }
+
+  override func tearDown() {
+    client = nil
+  }
+
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+}
