22f68cec4bbfd380548f7afab940a178e847ab48
TontonBuck <tontonbuck@live.fr>
2018-12-03 11:07:37 +0100

    Added SideMenu framework to project. Added SlideMenuNavigationController, which will be used to setup menu

---
+++ Controllers/SideMenuNavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  SideMenuNavigationController.swift
+//  Clic&Farm-iOS
+//
+//  Created by Guillaume Roux on 03/12/2018.
+//  Copyright © 2018 Ekylibre. All rights reserved.
+//
+
+import SideMenu
+
+class SideMenuNavigationController: UISideMenuNavigationController {
+
+  // MARK: - Initialization
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    sideMenuManager.menuPresentMode = .menuSlideIn
+  }
+}
